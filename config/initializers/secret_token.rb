63e6f66f69ae77659832883c0f385ae617ed688e
Vikram Haridas <vikram.haridas89@gmail.com>
2014-10-10 20:34:45 +0530

    Initialise repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cruder::Application.config.secret_token = 'e16e5b08a953a0eab60b98f9e7fb42ff5f4418f7e3b325e8f82ac9e228608e81c9d86e44925218f52b924653f641a01edc18b6cc2d1c000a2f41b7adbcba5468'
